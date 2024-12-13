2890a1cff77abf9b5d391015ba1f8e1b9f1c98b7
Tobias Abrahamsson <tobias.abrahamsson@zenseact.com>
2024-12-13 15:57:08 +0100

    updating solution for day5 task 2

--- 2024/05/day05_2.py
+++ 2024/05/day05_2.py
@@ -1,49 +1,23 @@
 #!/usr/bin/env python3
 
+import re
 
-def update_rules(rule: list, ordening_rules: dict) -> None:
-    first = ordening_rules.setdefault(rule[0], {'pre': [], 'post': []})
-    first['post'].append(rule[1])
-    second = ordening_rules.setdefault(rule[1], {'pre': [], 'post': []})
-    second['pre'].append(rule[0])
-
-def check_order(idx, update, rule) -> bool:
-    #print(f"idx = {idx}, update = {update}, rule = {rule}")
-    if idx < len(update)-1  and update[idx+1] not in rule['post']:
-        #print(f"{idx} < {len(update)} and {update[idx+1]} not in {rule['post']}")
-        return True
-
-    return False
-
-def invalidate_updates(updates: list[list], ordening_rules: dict) -> list[list]:
-    invalid_updates = []
-
-    for update in updates:
-        if any([check_order(idx, update, ordening_rules.get(number)) for idx, number in enumerate(update)]):
-            invalid_updates.append(update)
-
-    return invalid_updates
-
+from functools import cmp_to_key
+from pathlib import Path
 
 def main():
 
-    ordening_rules = {}
-    updates = []
-    with open('data.txt', 'r') as f:
-        for line in f:
-            line = line.strip()
-            if not line:
-                break
-            update_rules(line.split('|'), ordening_rules)
+    rules_txt, updates_txt = Path('data.txt').read_text().strip().split("\n\n")
+    rules = {tuple(numbers) for numbers in re.findall(r"(\d+)\|(\d+)", rules_txt)}
+    updates = [re.findall(r"\d+", update) for update in updates_txt.split("\n")]
 
-        for line in f:
-            updates.append(line.strip().split(','))
+    changed_medians_sum = 0
+    for update in updates:
+        fixed = sorted(update, key=cmp_to_key(lambda x, y: 1 if (y, x) in rules else -1))
+        if fixed != update:
+            changed_medians_sum += int(fixed[len(fixed) // 2])
 
-    invalid_updates = invalidate_updates(updates, ordening_rules)
-    print(f"Number of valid updates: {len(invalid_updates)}")
-
-    middle_sum = sum([int(update[(len(update)-1)//2]) for update in invalid_updates])
-    print(f"The sum is : {middle_sum}")
+    print(f"The sum is : {changed_medians_sum}")
 
     return 0
 
